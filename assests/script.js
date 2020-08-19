6c02652b6f3a561350010db30afc5812234cae1f
mkolek1015 <mkolek7@yahoo.com>
2020-08-18 20:55:25 -0400

    first push

---
+++ assests/script.js
@@ -0,0 +1,42 @@
+//generate random password
+function generate(){
+
+    //set password length/complexity
+    let complexity = document.getElementById("slider").value;
+
+    //p0ssible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    //create for loop to choose characters
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+        
+
+    //add password to textbox
+    document.getElementById("display").value = password;
+
+    //add password to previously generated passwords section
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+
+  }
+
+  // set default lenght display of 25
+  document.getElementById("length").innerHTML = "Length 25";
+
+  //function to set length based on slider position.
+  document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHtml = Length: 1";        
+    }
+
+  }
+
+
